e46887094b637c430ed674291942234306d12733
KotypeyPyEdition <mr.unionchik@gmail.com>
2019-11-03 22:32:08 +0200

    awfawfaw

--- bot/loops.py
+++ bot/loops.py
@@ -11,10 +11,10 @@ class Loops(commands.Cog):
         self.bot = bot
     @tasks.loop(seconds=5)
     def change_online(self):
+        response = requests.get('https://c.minase.tk/api/v1/onlineUsers')
+        online = response.json()['result']
         guild = globals.client.get_guild(config.server)
         channel = guild.get_channel(639131437932609556)
-        online = 0
-        response = requests.get('https://c.minase.tk/api/v1/onlineUsers').json()
-        online = response['result']
         print('changing online channel...')
         await channel.edit(name=f"Online: {online}")
+        
