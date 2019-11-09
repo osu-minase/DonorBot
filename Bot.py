e323f7971356477f697d94e9bfdf3f435d2c181b
KotypeyPyEdition <mr.unionchik@gmail.com>
2019-11-09 18:33:48 +0200

    requests -> redis test

--- Bot.py
+++ Bot.py
@@ -27,7 +27,7 @@ class Client(commands.Bot):
     async def is_donor(self, ctx: commands.Context):
         role = discord.utils.get(ctx.guild.roles, id=config.rid)
         if role not in ctx.author.roles:
-            return False
+            returёn False
         else:
             return True 
 
@@ -37,8 +37,11 @@ class Client(commands.Bot):
         print('Changinx online channel...')
         channel = await self.fetch_channel(639131437932609556)
         online = 0
-        response = requests.get('https://c.minase.tk/api/v1/onlineUsers').json()
-        online = response['result']
+        pubsub = glob.redis.pubsub()
+        pubsub.subscribe(['ripple:online_users'])
+        for i in pubsub.listen():
+
+            online = i['data']
         people = None
         if online % 10 > 4 and online % 10 < 10 or online % 10 == 1 or online % 10 == 0:
             people = 'человек'
--- globals.py
+++ globals.py
@@ -1,5 +1,6 @@
 from helpers import db
 import config
-
+import redis
 client = None
 db = db.Db(config.host, config.username, config.password, config.database, 1)
+redis = redis.Redis() # configure it yourself
